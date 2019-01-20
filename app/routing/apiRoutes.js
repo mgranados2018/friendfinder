607b899f49e6594398d3c45b2fa826174271bddb
firefly <11maguirre.a@gmail.com>
2019-01-19 23:46:02 -0600

    added logic to compare new entry with all entries; still having problems adding info from form to json file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,19 +4,36 @@ var friendsData = require("../data/friends");
 module.exports = function (app) {
 
     //use to display the json survey data to the user when clicking on the survey api link
-app.get("/api/survey", function (req, res) {
-    res.json(friendsData);
-})
+    app.get("/api/survey", function (req, res) {
+
+        res.json(friendsData);
+    })
 
 
-// write for loop that will compare what the user inputs to json object
+    // write for loop that will compare what the user inputs to json object
 
-// app.post("api/friends", function(req,res){
-//     var userdata = req.body;
+    app.post("/api/survey", function (req, res) {
+        var newfriend = req.body;
+        console.log(newfriend);
+
+        var totaldifference;
+
+        for(i=0;i<friendsData.length;i++){
+                     
+            var totaldifference = math.abs(friendsData.scores[i]-newfriend.scores[i])
+            console.log("total difference array"+totaldifference);
+       
+        }
+
+        function sort(){
+            totaldifference.sort(function(a,b){return a-b})
+        }
 
 
+        friendsData.push(req.body)
 
-// })
+
+    })
 
 
 }
